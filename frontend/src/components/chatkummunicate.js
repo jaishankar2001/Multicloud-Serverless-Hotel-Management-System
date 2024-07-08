6370276cb24213284cc2af28c2beddae06d0ef17
kdhola <ks328392@dal.ca>
2024-07-08 11:54:16 -0300

    resolved bug

--- frontend/src/components/chatkummunicate.js
+++ frontend/src/components/chatkummunicate.js
@@ -1,15 +1,25 @@
-// src/components/ChatKommunicate.js
 import { useEffect } from 'react';
 
 const ChatKommunicate = () => {
   useEffect(() => {
-    (function(d, m){
-      var kommunicateSettings = {"appId":"2884ca133c0d4bf9d92b56625c718dfe8","popupWidget":true,"automaticChatOpenOnNavigation":true};
-      var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
-      s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
-      var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
-      window.kommunicate = m; m._globals = kommunicateSettings;
-    })(document, window.kommunicate || {});
+    const loadKommunicate = () => {
+      if (!document.getElementById('kommunicate-script')) {
+        var kommunicateSettings = {"appId":"2884ca133c0d4bf9d92b56625c718dfe8","popupWidget":true,"automaticChatOpenOnNavigation":true};
+        var s = document.createElement("script");
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+        s.id = "kommunicate-script"; // Add an id to the script
+        var h = document.getElementsByTagName("head")[0];
+        h.appendChild(s);
+        window.kommunicate = window.kommunicate || {};
+        window.kommunicate._globals = kommunicateSettings;
+      } else {
+        console.log('Kommunicate script is already loaded.');
+      }
+    };
+
+    loadKommunicate();
   }, []);
 
   return null;
